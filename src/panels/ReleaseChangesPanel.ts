9e6de073ff449e300bda3bd6bf099d81661172ad
nicolasalarconrapela <nicolasalarconrapela@gmail.com>
2026-03-12 13:38:09 +0200

    feat(rel): agregamos iconos oficiales en sharing

--- src/panels/ReleaseChangesPanel.ts
+++ src/panels/ReleaseChangesPanel.ts
@@ -366,6 +366,27 @@ export class ReleaseChangesPanel {
               .cta-button.secondary:hover {
                   background: var(--vscode-button-secondaryHoverBackground);
               }
+
+              .cta-button svg {
+                  width: 16px;
+                  height: 16px;
+                  fill: currentColor;
+                  flex-shrink: 0;
+              }
+
+              .release-footer {
+                  margin-top: 60px;
+                  padding-top: 30px;
+                  border-top: 1px solid var(--vscode-panel-border);
+                  font-size: 0.9em;
+                  color: var(--vscode-descriptionForeground);
+                  line-height: 1.8;
+                  font-style: italic;
+              }
+
+              .release-footer p {
+                  margin-bottom: 10px;
+              }
           </style>
       </head>
       <body>
@@ -396,16 +417,20 @@ export class ReleaseChangesPanel {
                   ⭐ ${vscode.l10n.t('Rate and Favorite')}
               </a>
               <a href="${linkedinShareUrl}" class="cta-button secondary" target="_blank">
-                  🔗 ${vscode.l10n.t('Share on LinkedIn')}
+                  <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>LinkedIn</title><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg>
+                  ${vscode.l10n.t('Share on LinkedIn')}
               </a>
               <a href="${redditShareUrl}" class="cta-button secondary" target="_blank">
-                  🤖 ${vscode.l10n.t('Share on Reddit')}
+                  <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Reddit</title><path d="M12 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12 12 12 0 0 0 12-12A12 12 0 0 0 12 0zm5.01 4.744c.688 0 1.25.561 1.25 1.249a1.25 1.25 0 0 1-2.498.056l-2.597-.547-.8 3.747c1.824.07 3.48.632 4.674 1.488.308-.309.73-.491 1.207-.491.968 0 1.754.786 1.754 1.754 0 .716-.435 1.333-1.01 1.614a3.111 3.111 0 0 1 .042.52c0 2.694-3.13 4.87-7.004 4.87-3.874 0-7.004-2.176-7.004-4.87 0-.183.015-.366.043-.534A1.748 1.748 0 0 1 4.028 12c0-.968.786-1.754 1.754-1.754.463 0 .898.196 1.207.49 1.207-.883 2.878-1.43 4.744-1.487l.885-4.182a.342.342 0 0 1 .14-.197.35.35 0 0 1 .238-.042l2.906.617a1.214 1.214 0 0 1 1.108-.701zM9.25 12C8.561 12 8 12.562 8 13.25c0 .687.561 1.248 1.25 1.248.687 0 1.248-.561 1.248-1.249 0-.688-.561-1.249-1.249-1.249zm5.5 0c-.687 0-1.248.561-1.248 1.25 0 .687.561 1.248 1.249 1.248.688 0 1.249-.561 1.249-1.249 0-.688-.56-.1.249-1.249-1.249zm-5.466 3.99a.327.327 0 0 0-.231.094.33.33 0 0 0 0 .463c.842.842 2.484.913 2.961.913.477 0 2.105-.056 2.961-.913a.361.361 0 0 0 .029-.463.33.33 0 0 0-.464 0c-.547.533-1.684.73-2.512.73-.828 0-1.979-.196-2.512-.73a.326.326 0 0 0-.232-.095z"/></svg>
+                  ${vscode.l10n.t('Share on Reddit')}
               </a>
               <a href="${twitterShareUrl}" class="cta-button secondary" target="_blank">
-                  🐦 ${vscode.l10n.t('Share on X')}
+                  <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>X</title><path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"/></svg>
+                  ${vscode.l10n.t('Share on X')}
               </a>
               <a href="https://github.com/nicolasalarconrapela/an-favorites" class="cta-button secondary" target="_blank">
-                  🤝 ${vscode.l10n.t('Contribute')}
+                  <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.43.372.823 1.102.823 2.222 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
+                  ${vscode.l10n.t('Contribute')}
               </a>
           </div>
 
